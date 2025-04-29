4fbb221c07d2057ef9465b11de46a09e61362a1c
Polat <polat.hd7@gmail.comgit config --global user.email polat.hd7@gmail.com>
2025-04-30 00:18:27 +0300

    BaseDriver update

--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -19,6 +19,7 @@ public class BaseDriver {
         wait=new WebDriverWait(driver, Duration.ofSeconds(20));
         js=(JavascriptExecutor)driver;
 
+        driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
